cd89ca26788ed276ce64c254996e16d1f9093671
mrquincle <anne@almende.org>
2014-04-24 17:36:03 +0200

    Android build

---
+++ ios/cordova-style/com.almende.VirtualMemo/platforms/android/ant-build/VirtualMemo-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/home/anne/myworkspace/virtuele-memo/ios/cordova-style/com.almende.VirtualMemo/platforms/android/ant-build/VirtualMemo-debug-unaligned.apk : \
+/home/anne/myworkspace/virtuele-memo/ios/cordova-style/com.almende.VirtualMemo/platforms/android/ant-build/VirtualMemo.ap_ \
+/home/anne/myworkspace/virtuele-memo/ios/cordova-style/com.almende.VirtualMemo/platforms/android/ant-build/classes.dex \
+/home/anne/myworkspace/virtuele-memo/ios/cordova-style/com.almende.VirtualMemo/platforms/android/CordovaLib/ant-build/classes.jar \
---
+++ ios/cordova-style/com.almende.VirtualMemo/platforms/android/ant-gen/R.java.d
@@ -0,0 +1,14 @@
+/home/anne/myworkspace/virtuele-memo/ios/cordova-style/com.almende.VirtualMemo/platforms/android/ant-gen/com/almende/VirtualMemo/R.java \
+ : /home/anne/myworkspace/virtuele-memo/ios/cordova-style/com.almende.VirtualMemo/platforms/android/res/drawable/icon.png \
+/home/anne/myworkspace/virtuele-memo/ios/cordova-style/com.almende.VirtualMemo/platforms/android/res/drawable-ldpi/icon.png \
+/home/anne/myworkspace/virtuele-memo/ios/cordova-style/com.almende.VirtualMemo/platforms/android/res/drawable-mdpi/icon.png \
+/home/anne/myworkspace/virtuele-memo/ios/cordova-style/com.almende.VirtualMemo/platforms/android/res/drawable-hdpi/icon.png \
+/home/anne/myworkspace/virtuele-memo/ios/cordova-style/com.almende.VirtualMemo/platforms/android/res/xml/config.xml \
+/home/anne/myworkspace/virtuele-memo/ios/cordova-style/com.almende.VirtualMemo/platforms/android/res/drawable-xhdpi/icon.png \
+/home/anne/myworkspace/virtuele-memo/ios/cordova-style/com.almende.VirtualMemo/platforms/android/res/values/strings.xml \
+/home/anne/myworkspace/virtuele-memo/ios/cordova-style/com.almende.VirtualMemo/platforms/android/ant-build/res/drawable/icon.png \
+/home/anne/myworkspace/virtuele-memo/ios/cordova-style/com.almende.VirtualMemo/platforms/android/ant-build/res/drawable-ldpi/icon.png \
+/home/anne/myworkspace/virtuele-memo/ios/cordova-style/com.almende.VirtualMemo/platforms/android/ant-build/res/drawable-mdpi/icon.png \
+/home/anne/myworkspace/virtuele-memo/ios/cordova-style/com.almende.VirtualMemo/platforms/android/ant-build/res/drawable-hdpi/icon.png \
+/home/anne/myworkspace/virtuele-memo/ios/cordova-style/com.almende.VirtualMemo/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
+/home/anne/myworkspace/virtuele-memo/ios/cordova-style/com.almende.VirtualMemo/platforms/android/ant-build/AndroidManifest.xml \
