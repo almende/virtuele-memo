4ff33e11d8e9cc68d6801711ea3b46ba650ef561
Stefan Kroon <stefan@dollie.local>
2014-04-24 17:34:36 +0200

    For Anne also the empty directories

---
+++ ios/Virtuele Memo/Virtuele Memo/VMOHuntViewController.h
@@ -0,0 +1,13 @@
+//
+//  VMOHuntViewController.h
+//  Virtuele Memo
+//
+//  Created by Stefan Kroon on 17-04-14.
+//  Copyright (c) 2014 Stefan Kroon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VMOHuntViewController : UIViewController
+
+@end
